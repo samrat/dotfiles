38b98edce17679e60d7fdf36ed71a38a42264007
Samrat Man Singh <samratmansingh@gmail.com>
2012-12-22 23:11:59 +0545

    Music!

---
+++ .xbindkeysrc.scm
@@ -0,0 +1,11 @@
+(map (lambda (binding) (xbindkey (car binding) (cdr binding)))
+     (list 
+           ;; Volume
+           '(XF86AudioRaiseVolume . "amixer set Master playback 2+")
+           '(XF86AudioLowerVolume . "amixer set Master playback 2-")
+           '(XF86AudioMute . "amixer set Master toggle")
+           ;; Play/pause
+           '(XF86AudioPlay . "mpc toggle")
+           '(XF86AudioNext . "mpc next")
+           '(XF86AudioPrev . "mpc prev")
+           ))
