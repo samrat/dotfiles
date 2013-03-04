27a9b57766d0bf01d8163e989956864c55dd6a5e
Samrat Man Singh <samratmansingh@gmail.com>
2013-03-05 00:12:07 +0545

    Clean up org-mode config.

--- .emacs.d/config/samrat-org.el
+++ .emacs.d/config/samrat-org.el
@@ -1,41 +1,16 @@
 (require 'org-latex)
 (setq org-directory "~/Dropbox/org")
-(setq org-agenda-files (list (concat org-directory "/projects.org")
-                             (concat org-directory "/home.org")))
-(setq org-default-notes-file (concat org-directory "/notes.org"))
+(setq org-agenda-files (list org-directory))
 
 (setq org-capture-templates
-      '(("t" "Todo" entry (file+headline "~/org/gtd.org" "Tasks")
-         "* TODO %?\n %i\n %a")
-        ("m" "Media" entry (file "~/org/media.org" "Media")
-         "* TODO %?\n %i\n %a")
-        ("j" "Journal" entry (file+datetree
-                              "~/org/journal.org") "* %?\nEntered on %U\n  %i\n  %a")))
+      '(("t" "Todo" entry (file+headline "todo.org" "Unsorted") "* TODO %i%?")
+        ("n" "Notes" entry (file+headline "notes.org" "Notes") "** %? ")
+        ("j" "Journal" entry (file+datetree "journal.org") "* %i%?")))
+
 (setq org-log-done t)
 (setq org-startup-indented t)
 
-;; 'djcb-org-article' for export org documents to the LaTex 'article', using
-;; XeTeX and some fancy fonts; requires XeTeX (see org-latex-to-pdf-process)
-(add-to-list 'org-export-latex-classes
-  '("article"
-"\\documentclass[11pt,a4paper]{article}
-\\usepackage[T1]{fontenc}
-\\usepackage{fontspec}
-\\usepackage{graphicx} 
-\\defaultfontfeatures{Mapping=tex-text}
-\\setmonofont[Scale=0.8]{DejaVu Sans Mono}
-\\usepackage{geometry}
-\\title{}
-      [NO-DEFAULT-PACKAGES]
-      [NO-PACKAGES]"
-     ("\\section{%s}" . "\\section*{%s}")
-     ("\\subsection{%s}" . "\\subsection*{%s}")
-     ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-     ("\\paragraph{%s}" . "\\paragraph*{%s}")
-     ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
-
-(setq org-latex-to-pdf-process 
-  '("xelatex -interaction nonstopmode %f"
-    "xelatex -interaction nonstopmode %f")) ;; for multiple passes
+(setq org-refile-targets `((,(concat "~" (user-login-name)
+                                     "/org/todo.org") . (:level . 1))))
 
 (provide 'samrat-org)
